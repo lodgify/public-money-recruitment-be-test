72dc7735fa71164bf52381453809581c7407205f
gapeevdi <gapeevdirog@gmail.com>
2021-09-23 14:17:29 +0300

    added RentalRepository to VacationRental.Infrastructure

---
+++ VacationRental.Application/Exceptions/InfrastructureException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VacationRental.Application.Exceptions
+{
+    public abstract class InfrastructureException : Exception
+    {
+        protected InfrastructureException()
+        {
+            
+        }
+
+        protected InfrastructureException(string message) : base(message)
+        {
+
+        }
+
+        protected InfrastructureException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
---
+++ VacationRental.Infrastructure/Persist/RentalRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using VacationRental.Domain.Entities;
+using VacationRental.Domain.Repositories;
+using VacationRental.Domain.Values;
+using VacationRental.Infrastructure.Persist.Exceptions;
+
+namespace VacationRental.Infrastructure.Persist
+{
+    public sealed class RentalRepository : IRentalRepository
+    {
+        private readonly Dictionary<RentalId, Rental> _rentals = new Dictionary<RentalId, Rental>();
+
+        public Rental Get(RentalId id)
+        {
+            if (_rentals.TryGetValue(id, out var rental))
+            {
+                return rental;
+            }
+
+            throw new RentalNotFoundException(id);
+        }
+    }
+}
