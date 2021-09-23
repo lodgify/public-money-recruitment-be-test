66a8b09e5fea107be8c24fcd2dabeeaec4eb9b2a
gapeevdi <gapeevdirog@gmail.com>
2021-09-23 15:38:42 +0300

    added CreateRental Command to VacationRental.Application

---
+++ VacationRental.Application/Commands/Rental/CreateRentalCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using VacationRental.Domain.Repositories;
+
+namespace VacationRental.Application.Commands.Rental
+{
+    public class CreateRentalCommand
+    {
+
+        private readonly IRentalRepository _rentalRepository;
+        private readonly ILogger<CreateRentalCommand> _logger;
+
+        public CreateRentalCommand(IRentalRepository rentalRepository,  ILogger<CreateRentalCommand> logger)
+        {
+            _rentalRepository = rentalRepository ?? throw new ArgumentNullException(nameof(rentalRepository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+
+        public async Task Handle(CreateRentalCommand request)
+        {
+            
+        }
+    }
+}
---
+++ VacationRental.Application/Commands/Rental/CreateRentalRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace VacationRental.Application.Commands.Rental
+{
+    public class RentalBindingModel
+    {
+        public int Units { get; set; }
+        public int PreparationTimeInDays { get; set; }
+    }
+}
