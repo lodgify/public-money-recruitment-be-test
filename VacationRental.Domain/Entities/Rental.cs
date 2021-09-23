f463135878a49d5c2039b72fb3681201ca0c2392
gapeevdi <gapeevdirog@gmail.com>
2021-09-23 13:10:24 +0300

    added RentalId, Rental to VacationRental.Domain

---
+++ VacationRental.Domain/Entities/Rental.cs
@@ -0,0 +1,16 @@
+﻿using VacationRental.Domain.Values;
+
+namespace VacationRental.Domain.Entities
+{
+    public class Rental
+    {
+        public Rental(RentalId id, int units)
+        {
+            Id = id;
+            Units = units;
+        }
+
+        public RentalId Id { get; }
+        public int Units { get; }
+    }
+}
---
+++ VacationRental.Domain/Values/RentalId.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using VacationRental.Domain.Common;
+
+namespace VacationRental.Domain.Values
+{
+    public sealed class RentalId : ValueObject<RentalId>
+    {
+        public RentalId(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; }
+
+        protected override IEnumerable<object> GetAttributesToIncludeInEqualityCheck() => new List<object>{Id};
+    }
+}
