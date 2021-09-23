e76283247a86a75390a8920ae8e6ad8df6716a51
gapeevdi <gapeevdirog@gmail.com>
2021-09-23 13:45:12 +0300

    added IRentalRepository

---
+++ VacationRental.Domain/Repositories/IRentalRepository.cs
@@ -0,0 +1,10 @@
+﻿using VacationRental.Domain.Entities;
+using VacationRental.Domain.Values;
+
+namespace VacationRental.Domain.Repositories
+{
+    public interface IRentalRepository
+    {
+        Rental Get(RentalId id);
+    }
+}
