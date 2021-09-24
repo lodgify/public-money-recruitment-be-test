3f397b37096c00169798a268ffaaff14567a5157
gapeevdi <gapeevdirog@gmail.com>
2021-09-24 10:24:05 +0300

    VacationRental.Application : implemented IRequestHandler in RentalByIdQueryHandler

---
+++ VacationRental.Application/Queries/Rental/GetRentalByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace VacationRental.Application.Queries.Rental
+{
+    public sealed class GetRentalByIdQuery : IRequest<RentalViewModel>
+    {
+        public GetRentalByIdQuery(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; }
+    }
+}
--- VacationRental.Application/Queries/Rental/RentalByIdQueryHandler.cs
+++ VacationRental.Application/Queries/Rental/RentalByIdQueryHandler.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
+using MediatR;
 using VacationRental.Domain.Repositories;
 using VacationRental.Domain.Values;
 
 namespace VacationRental.Application.Queries.Rental
 {
-    public class RentalByIdQueryHandler
+    public class RentalByIdQueryHandler : IRequestHandler<GetRentalByIdQuery, RentalViewModel>
     {
         private readonly IRentalRepository _rentalRepository;
 
@@ -14,7 +16,7 @@ namespace VacationRental.Application.Queries.Rental
             _rentalRepository = rentalRepository ?? throw new ArgumentNullException(nameof(rentalRepository));
         }
 
-        public async Task<RentalViewModel> Handle(GetRentalById query)
+        public async Task<RentalViewModel> Handle(GetRentalByIdQuery query, CancellationToken cancellationToken)
         {
             var domainModel = _rentalRepository.Get(new RentalId(query.Id));
             return null;
