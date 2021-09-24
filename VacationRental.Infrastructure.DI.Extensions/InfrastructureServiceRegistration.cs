de186482f1bf99a0b9e43b1e870a77b5137fc688
gapeevdi <gapeevdirog@gmail.com>
2021-09-24 13:44:22 +0300

    Created VacationRental.Infra.DI.Extensions

---
+++ VacationRental.Infrastructure.DI.Extensions/InfrastructureServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using VacationRental.Domain.Repositories;
+using VacationRental.Infrastructure.Persist;
+
+namespace VacationRental.Infrastructure.DI.Extensions
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddScoped<IBookingRepository, BookingRepository>();
+            services.AddScoped<IRentalRepository, RentalRepository>();
+
+            return services;
+        }
+    }
+}
