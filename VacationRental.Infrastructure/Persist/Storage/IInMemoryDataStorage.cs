bfe67a20a0c7d301a17ded67eb1376d762eac101
gapeevdi <gapeevdirog@gmail.com>
2021-09-24 15:57:00 +0300

    VacationRental.Infrastructure : added InMemoryDataStorage

---
+++ VacationRental.Infrastructure/Persist/Storage/IInMemoryDataStorage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VacationRental.Infrastructure.Persist.Storage
+{
+    public interface IInMemoryDataStorage<TModel>
+    where TModel : class, new()
+    {
+        TModel Add(TModel model);
+        bool TryGetAsync(int Key, out TModel model);
+        IReadOnlyCollection<TModel> Get(Func<TModel, bool> specification);
+    }
+
+}
---
+++ VacationRental.Infrastructure/Persist/Storage/InMemoryDataStorage.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace VacationRental.Infrastructure.Persist.Storage
+{
+
+    public sealed class InMemoryDataStorage<TModel> : IInMemoryDataStorage<TModel>
+        where TModel : class, new()
+    {
+
+        private int _lastKeyValue = 0;
+        private readonly Dictionary<int, TModel> _items = new Dictionary<int, TModel>();
+        private readonly Action<TModel, int> _setNextKey;
+
+        public InMemoryDataStorage(Expression<Func<TModel, int>> keyPropertyExpression)
+        {
+            _setNextKey = CompileKeySetter(keyPropertyExpression);
+        }
+
+        public TModel Add(TModel model)
+        {
+            var nextKey = _lastKeyValue++;
+            _setNextKey(model, nextKey);
+            _items.Add(nextKey, model);
+
+            return model;
+        }
+
+        public bool TryGetAsync(int key, out TModel model) =>
+            _items.TryGetValue(key, out model);
+
+        public IReadOnlyCollection<TModel> Get(Func<TModel, bool> specification) =>
+            _items.Values.Where(specification).ToList();
+
+
+        /// <summary>
+        /// Generates a lambda setting a key value for an item.
+        /// </summary>
+        /// <param name="keyPropertyExpression"></param>
+        /// <returns></returns>
+        private Action<TModel, int> CompileKeySetter(Expression<Func<TModel, int>> keyPropertyExpression)
+        {
+            var keyPropertyInfo = (PropertyInfo)(keyPropertyExpression.Body as MemberExpression).Member;
+            var itemParameterExpression = Expression.Parameter(typeof(TModel));
+            var newKeyParameterExpression = Expression.Parameter(typeof(int));
+
+            var assignKeyExpression = Expression.Assign(Expression.Property(itemParameterExpression, keyPropertyInfo),
+                newKeyParameterExpression);
+
+            var lambda = Expression.Lambda<Action<TModel, int>>(assignKeyExpression, itemParameterExpression,
+                newKeyParameterExpression);
+
+            return lambda.Compile();
+        }
+    }
+}
