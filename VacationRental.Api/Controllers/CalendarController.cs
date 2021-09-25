f85dccca3769c02cd53472ba6756292d30434df9
gapeevdi <gapeevdirog@gmail.com>
2021-09-25 17:09:15 +0300

    VacationRental.API : deleted commented code from controllers

--- VacationRental.Api/Controllers/CalendarController.cs
+++ VacationRental.Api/Controllers/CalendarController.cs
@@ -18,42 +18,6 @@ namespace VacationRental.Api.Controllers
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
         }
 
-        //[HttpGet]
-        //public CalendarViewModel Get(int rentalId, DateTime start, int nights)
-        //{
-        //    if (nights < 0)
-        //        throw new ApplicationException("Nights must be positive");
-        //    if (!_rentals.ContainsKey(rentalId))
-        //        throw new ApplicationException("Rental not found");
-
-        //    var result = new CalendarViewModel 
-        //    {
-        //        RentalId = rentalId,
-        //        Dates = new List<CalendarDateViewModel>() 
-        //    };
-        //    for (var i = 0; i < nights; i++)
-        //    {
-        //        var date = new CalendarDateViewModel
-        //        {
-        //            Date = start.Date.AddDays(i),
-        //            Bookings = new List<CalendarBookingViewModel>()
-        //        };
-
-        //        foreach (var booking in _bookings.Values)
-        //        {
-        //            if (booking.RentalId == rentalId
-        //                && booking.Start <= date.Date && booking.Start.AddDays(booking.Nights) > date.Date)
-        //            {
-        //                date.Bookings.Add(new CalendarBookingViewModel { Id = booking.Id });
-        //            }
-        //        }
-
-        //        result.Dates.Add(date);
-        //    }
-
-        //    return result;
-        //}
-
         [HttpGet]
         public async Task<CalendarViewModel> Get(int rentalId, DateTime start, int nights)
         {
--- VacationRental.Api/Controllers/RentalsController.cs
+++ VacationRental.Api/Controllers/RentalsController.cs
@@ -19,37 +19,12 @@ namespace VacationRental.Api.Controllers
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
         }
 
-        //[HttpGet]
-        //[Route("{rentalId:int}")]
-        //public RentalViewModel Get(int rentalId)
-        //{
-        //    if (!_rentals.ContainsKey(rentalId))
-        //        throw new ApplicationException("Rental not found");
-
-        //    return _rentals[rentalId];
-        //}
-
-        //[HttpPost]
-        //public ResourceIdViewModel Post(RentalBindingModel model)
-        //{
-        //    var key = new ResourceIdViewModel { Id = _rentals.Keys.Count + 1 };
-
-        //    _rentals.Add(key.Id, new RentalViewModel
-        //    {
-        //        Id = key.Id,
-        //        Units = model.Units
-        //    });
-
-        //    return key;
-        //}
-
         [HttpGet]
         [Route("{rentalId:int}")]
         public async Task<RentalViewModel> Get(int rentalId)
         {
             return await _mediator.Send(new GetRentalByIdQuery(rentalId));
         }
-
 
         [HttpPost]
         public async Task<ResourceIdViewModel> Post(CreateRentalRequest request)
