b04ae42cdc5beb34bd61548dcee94411ce17a9fe
nabbo <nabbo247@hotmail.com>
2022-03-19 19:42:50 +0100

    Added data access layer to project

---
+++ VacationRental.DataAccessLayer/Configs/BookingConfig.cs
@@ -0,0 +1,25 @@
+﻿using VacationRental.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace VacationRental.SqlDataAccess.Configs
+{
+	/// Entity Framework map the table design to the Booking model, all Booking table schema specifications should be defined here.
+	public class BookingConfig : IEntityTypeConfiguration<Booking>
+	{
+		public void Configure(EntityTypeBuilder<Booking> builder)
+		{
+			builder.ToTable("booking");
+
+			builder.Property(f => f.Id)
+				.ValueGeneratedOnAdd();
+			builder.Property(f => f.Nights);
+			builder.Property(f => f.RentalId);
+			builder.Property(f => f.Start);
+			builder.Property(f => f.IsPreparationTime)
+				.HasDefaultValue(false);
+
+			builder.HasKey(f => f.Id);
+		}
+	}
+}
---
+++ VacationRental.DataAccessLayer/Module/DomainSqlDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using VacationRental.Domain;
+using VacationRental.Domain.Models;
+
+namespace VacationRental.SqlDataAccess.Module
+{
+	public class DomainSqlDataAccess : Autofac.Module
+	{
+		/// Overriden Load function to add registrations to the container.
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterType<EFCRepository<Booking>>().As<GIRepository<Booking>>();
+			builder.RegisterType<EFCRepository<Rental>>().As<GIRepository<Rental>>();
+		}
+	}
+}
