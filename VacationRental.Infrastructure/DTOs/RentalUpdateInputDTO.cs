a88876555a29946d9a637a10106164dd6ccf7bd0
Nurullo Alijonov <nurullo.alijonov@oplog.io>
2022-04-24 17:47:17 +0300

    add validations

--- VacationRental.Infrastructure/DTOs/RentalUpdateInputDTO.cs
+++ VacationRental.Infrastructure/DTOs/RentalUpdateInputDTO.cs
@@ -1,4 +1,5 @@
-﻿using Mapster;
+﻿using FluentValidation;
+using Mapster;
 using VacationRental.Domain.Rentals;
 
 namespace VacationRental.Infrastructure.DTOs
@@ -21,6 +22,15 @@ namespace VacationRental.Infrastructure.DTOs
             config.ForType<RentalUpdateInputDTO, Rental>()
                 .Map(dest => dest.Units, src => src.Units)
                 .Map(dest => dest.PreparationTime, src => src.PreparationTimeInDays);
+        }
+    }
+
+    public class RentalUpdateInputDTOValidator : AbstractValidator<RentalUpdateInputDTO>
+    {
+        public RentalUpdateInputDTOValidator()
+        {
+            RuleFor(x => x.Units).GreaterThan(0).WithMessage("Rental should at least have 1 Unit");
+            RuleFor(x => x.PreparationTimeInDays).GreaterThan(0).WithMessage("Preparation Time cannot be less than 1 days");
         }
     }
 }
