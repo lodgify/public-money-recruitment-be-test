36107a3bfdb3b3de5d11c9714b7242b8986c0357
GaletskyVladimir <galezkij@gmail.com>
2022-09-02 17:49:51 +0300

    Added unit tests

---
+++ VacationRental.Unit.Tests/Application/BookingConflictResolverTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Linq;
+using VacationRental.Unit.Tests.Stubs;
+using VR.Application.Resolvers;
+using VR.Domain.Models;
+using VR.Infrastructure.Exceptions;
+using Xunit;
+
+namespace VacationRental.Unit.Tests.Application
+{
+    public class BookingConflictResolverTests
+    {
+        private readonly IBookingConflictResolver _bookingConflictResolver;
+
+        public BookingConflictResolverTests()
+        {
+            _bookingConflictResolver = new BookingConflictResolver();
+        }
+
+        [Fact]
+        public void GetAvailableUnit_ShouldReturnAvailableUnitNumber()
+        {
+            var rental = new Rental()
+            {
+                PreparationTimeInDays = 2,
+                Units = 4
+            };
+
+            var availableUnit = _bookingConflictResolver.GetAvailableUnit(rental, BookingStubs.GetRentalBookingsWithThirdUnitAvailable());
+
+            Assert.Equal(3, availableUnit);
+        }
+
+        [Fact]
+        public void GetAvailableUnit_ShouldReturnExceptionIfNoAvailableUnits()
+        {
+            var rental = new Rental()
+            {
+                PreparationTimeInDays = 2,
+                Units = 4
+            };
+
+            Assert.Throws<BookingConflictsException>(() => _bookingConflictResolver.GetAvailableUnit(rental, BookingStubs.GetRentalBookingsWithoutAvailableUnit()));
+        }
+
+        [Fact]
+        public void GetCrossBookedUnits_ShouldReturnCrossedBookings()
+        {
+            var rental = new Rental()
+            {
+                Id = 1,
+                PreparationTimeInDays = 4,
+                Units = 10
+            };
+
+            var currentBooking = new Booking()
+            {
+                Id = 1,
+                RentalId = 1,
+                Start = new DateTime(2022, 9, 10),
+                Nights = 5
+            };
+
+            var crossedBookings = _bookingConflictResolver.GetCrossBookedUnits(rental, currentBooking, BookingStubs.GetRentalPossibleCrossBookedUnits()).ToList();
+
+            Assert.Equal(BookingStubs.GetRentalCrossBookedUnits().Select(booking => booking.Id), crossedBookings.Select(booking => booking.Id));
+        }
+
+        [Fact]
+        public void HasBookingConflicts_ShouldReturnTrueIfUnitsNumberNotEnought()
+        {
+            var rental = new Rental()
+            {
+                Id = 1,
+                PreparationTimeInDays = 1,
+                Units = 1
+            };
+
+            var hasConflict = _bookingConflictResolver.HasBookingConflicts(rental, BookingStubs.GetRentalConflictBookings());
+
+            Assert.True(hasConflict);
+        }
+
+        [Fact]
+        public void HasBookingConflicts_ShouldReturnTrueIfPreparationDaysConflict()
+        {
+            var rental = new Rental()
+            {
+                Id = 1,
+                PreparationTimeInDays = 4,
+                Units = 2
+            };
+
+            var hasConflict = _bookingConflictResolver.HasBookingConflicts(rental, BookingStubs.GetRentalConflictBookings());
+
+            Assert.True(hasConflict);
+        }
+
+        [Fact]
+        public void HasBookingConflicts_ShouldReturnFalseIfNoBookingConflicts()
+        {
+            var rental = new Rental()
+            {
+                Id = 1,
+                PreparationTimeInDays = 2,
+                Units = 2
+            };
+
+            var hasConflict = _bookingConflictResolver.HasBookingConflicts(rental, BookingStubs.GetRentalConflictBookings());
+
+            Assert.False(hasConflict);
+        }
+    }
+}
---
+++ VacationRental.Unit.Tests/Stubs/BookingStubs.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VR.Domain.Models;
+
+namespace VacationRental.Unit.Tests.Stubs
+{
+    public class BookingStubs
+    {
+        public static List<Booking> GetRentalBookingsWithThirdUnitAvailable()
+        {
+            return new List<Booking>()
+            {
+                new Booking()
+                {
+                    Id = 1,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 1),
+                    Nights = 2,
+                    Unit = 1
+                },
+                new Booking()
+                {
+                    Id = 2,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 1),
+                    Nights = 4,
+                    Unit = 2
+                },
+                new Booking()
+                {
+                    Id = 3,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 1),
+                    Nights = 3,
+                    Unit = 4
+                }
+            };
+        }
+
+        public static List<Booking> GetRentalBookingsWithoutAvailableUnit()
+        {
+            return new List<Booking>()
+            {
+                new Booking()
+                {
+                    Id = 1,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 1),
+                    Nights = 2,
+                    Unit = 1
+                },
+                new Booking()
+                {
+                    Id = 2,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 1),
+                    Nights = 4,
+                    Unit = 2
+                },
+                new Booking()
+                {
+                    Id = 3,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 1),
+                    Nights = 3,
+                    Unit = 3
+                },
+                new Booking()
+                {
+                    Id = 4,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 1),
+                    Nights = 1,
+                    Unit = 4
+                }
+            };
+        }
+
+        public static List<Booking> GetRentalPossibleCrossBookedUnits()
+        {
+            return new List<Booking>()
+            {
+                new Booking()
+                {
+                    Id = 2,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 4),
+                    Nights = 2,
+                    Unit = 2
+                },
+                new Booking()
+                {
+                    Id = 3,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 7),
+                    Nights = 5,
+                    Unit = 3
+                },
+                new Booking()
+                {
+                    Id = 4,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 7),
+                    Nights = 10,
+                    Unit = 4
+                },
+                new Booking()
+                {
+                    Id = 5,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 11),
+                    Nights = 1,
+                    Unit = 5
+                },
+                new Booking()
+                {
+                    Id = 6,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 11),
+                    Nights = 7,
+                    Unit = 6
+                },
+                new Booking()
+                {
+                    Id = 7,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 16),
+                    Nights = 2,
+                    Unit = 7
+                },
+                new Booking()
+                {
+                    Id = 8,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 5),
+                    Nights = 5,
+                    Unit = 8
+                },
+                new Booking()
+                {
+                    Id = 9,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 10),
+                    Nights = 5,
+                    Unit = 9
+                },
+                new Booking()
+                {
+                    Id = 10,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 15),
+                    Nights = 3,
+                    Unit = 10
+                },
+            };
+        }
+
+        public static List<Booking> GetRentalCrossBookedUnits()
+        {
+            return new List<Booking>()
+            {
+                new Booking()
+                {
+                    Id = 3,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 7),
+                    Nights = 5,
+                    Unit = 3
+                },
+                new Booking()
+                {
+                    Id = 4,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 7),
+                    Nights = 10,
+                    Unit = 4
+                },
+                new Booking()
+                {
+                    Id = 5,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 11),
+                    Nights = 1,
+                    Unit = 5
+                },
+                new Booking()
+                {
+                    Id = 6,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 11),
+                    Nights = 7,
+                    Unit = 6
+                },
+                new Booking()
+                {
+                    Id = 7,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 16),
+                    Nights = 2,
+                    Unit = 7
+                },
+                new Booking()
+                {
+                    Id = 8,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 5),
+                    Nights = 5,
+                    Unit = 8
+                },
+                new Booking()
+                {
+                    Id = 9,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 10),
+                    Nights = 5,
+                    Unit = 9
+                },
+                new Booking()
+                {
+                    Id = 10,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 15),
+                    Nights = 3,
+                    Unit = 10
+                },
+            };
+        }
+
+        public static List<Booking> GetRentalConflictBookings()
+        {
+            return new List<Booking>()
+            {
+                new Booking()
+                {
+                    Id = 1,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 5),
+                    Nights = 2,
+                    Unit = 1
+                },
+                new Booking()
+                {
+                    Id = 2,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 1),
+                    Nights = 4,
+                    Unit = 2
+                },
+                new Booking()
+                {
+                    Id = 3,
+                    RentalId = 1,
+                    Start = new DateTime(2022, 9, 1),
+                    Nights = 2,
+                    Unit = 1
+                }
+            };
+        }
+    }
+}
