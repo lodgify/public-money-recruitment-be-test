99664f33bb520f6ff99e86b2985554b58a1131fd
GaletskyVladimir <galezkij@gmail.com>
2022-09-02 17:44:14 +0300

    Created Domain layer

---
+++ VR.Domain/Models/Booking.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VR.Domain.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+
+        public int RentalId { get; set; }
+
+        public Rental Rental { get; set; }
+
+        public int Unit { get; set; }
+
+        public DateTime Start
+        {
+            get => _startIgnoreTime;
+            set => _startIgnoreTime = value.Date;
+        }
+
+        private DateTime _startIgnoreTime;
+        public DateTime End { get => Start.AddDays(Nights); }
+        public int Nights { get; set; }
+    }
+}
---
+++ VR.Domain/Models/Rental.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace VR.Domain.Models
+{
+    public class Rental
+    {
+        public Rental()
+        {
+            Bookings = new List<Booking>();
+        }
+
+        public int Id { get; set; }
+
+        public int Units { get; set; }
+
+        public int PreparationTimeInDays { get; set; }
+
+        public ICollection<Booking> Bookings { get; set; }
+    }
+}
